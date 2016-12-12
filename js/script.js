74ae1fae47b38cf5c86779c8bc3ef8c26b2ee1e3
lamynes <lamynes.pl@gmail.com>
2016-12-12 15:49:45 -0800

    Create js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+        ////////////////////////////////////
         // animate.js
         document.write('<link href="css/animate.min.css" rel="stylesheet">');
+        
+        $(function() {
+            setTimeout(function() {
+                $('body').addClass('loaded');
+            }, 0);
+        });
     
