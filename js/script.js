4129ae4d131fc9599c14ff879e026eb94ff522a9
lamynes <lamynes.pl@gmail.com>
2016-12-13 21:08:06 -0800

    Create js/script.js

--- js/script.js
+++ js/script.js
@@ -8,4 +8,44 @@
                 $('body').addClass('loaded');
             }, 0);
         });
+        
+        
+    //////////////////////////////////// 
+// Hover.js 
+document.write ('<link href = "./css/hover-min.css"rel="stylesheet">');
+////////////////////////////////////
+
+$(document).ready(function(){
+    $('.headline').hover(function(){
+        $(this).addClass('animated bounce');
+    });
+});
+
+
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);        
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);         
+        });
+}
+
+function animationClick(element, animation){
+    element = $(element);
+    element.click(
+        function() {
+            element.addClass('animated ' + animation);        
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);         
+  
+        });
+}
     
