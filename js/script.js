c09e82cf160411c1e56f4328f9198f8ed5578df1
lamynes <lamynes.pl@gmail.com>
2017-01-19 23:50:52 -0800

    Create js/script.js

--- js/script.js
+++ js/script.js
@@ -32,4 +32,17 @@ $(function(){
     });
 });
 //////////////////////////////////////////
+    ////////////////////////////////////
+// hover.js
+document.write('<link href="css/hover-min.css" rel="stylesheet">');
+////////////////////////////////////
+    document.write('<scr'+'ipt type="text/javascript" charset="utf-8" src="jquery.tubular.1.0.js"></scr'+'ipt>');
+/*$(function(){
+    // add the video on the body
+    $('body').tubular({videoId: 'ab0TSkLe-E0', start: 3});  
+    // keep the main container clickable
+    $('.background').css('z-index', 10);
+    // make the video take the full window
+    $('body').css('padding', 0);
+})*/
     
